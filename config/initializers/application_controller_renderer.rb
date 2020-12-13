7c215f40ba4702aa078bb5f10f277cf956a7fff3
Skanayama <74477869+Skanayama@users.noreply.github.com>
2020-12-13 23:26:09 +0900

    開発環境、ユーザー周り

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+Refile.secret_key = 'a21693ba08914b6c0cf45fbaddf84a7775ffe37fa78f8d4a6a3a1531cf1b212512d9e7d814b0af48b70169aff7c8cc51384cdbce7dce242f690fad32daf92d9c'
